2d33cc9ffb96b74da4d97ec4fe6ef4d3708c752a
renanll <renan_ll@hotmail.com>
2022-01-26 23:50:32 -0300

    CHORES:Centralização e dimensionamento do pipeline

--- DataCampMachineLearn/Supervised/Pre-Processing.py
+++ DataCampMachineLearn/Supervised/Pre-Processing.py
@@ -1,5 +1,6 @@
 # %%
 # Import pandas
+from sklearn.preprocessing import scale
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Imputer
 from sklearn.svm import SVC
@@ -95,3 +96,15 @@ y_pred = pipeline.predict(X_test)
 print(classification_report(y_test, y_pred))
 
 # %%
+# Import scale
+
+# Scale the features: X_scaled
+X_scaled = scale(X)
+
+# Print the mean and standard deviation of the unscaled features
+print("Mean of Unscaled Features: {}".format(np.mean(X)))
+print("Standard Deviation of Unscaled Features: {}".format(np.std(X)))
+
+# Print the mean and standard deviation of the scaled features
+print("Mean of Scaled Features: {}".format(np.mean(X_scaled)))
+print("Standard Deviation of Scaled Features: {}".format(np.std(X_scaled)))
